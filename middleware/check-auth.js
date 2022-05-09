c1f8fa8c2da7162514529b816631dce7470591f4
kanzariyaviral <kanzariyaviral2312002@gmail.com>
2022-05-09 10:50:57 +0530

    node-api-shop with email send and verifiy user with email

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,19 +1,19 @@
-const {JWK_KEY}=require('../config/config')
-const jwt =require('jsonwebtoken');
+const { JWK_KEY } = require('../config/config')
+const jwt = require('jsonwebtoken');
 
-module.exports=(req,res,next)=>{
-    try{
-        const token=req.headers.authorization.split(" ")[1];
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
         // console.log(token)
-        const decoded=jwt.verify(token,JWK_KEY);
-        req.userdata=decoded;
+        const decoded = jwt.verify(token, JWK_KEY);
+        req.userdata = decoded;
         next();
     }
-    catch(error){
+    catch (error) {
         console.log(error)
         return res.status(401).json({
-            
-            message:'Auth failed'
+
+            message: 'Auth failed'
         })
     }
 }
